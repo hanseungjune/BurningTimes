98c20d144e4adec09b07b71a65375f38c43fde46
배상준 <qotkdwns1324@naver.com>
2022-11-17 11:51:33 +0900

    movies like

--- back/final-pjt-back/movies/urls.py
+++ back/final-pjt-back/movies/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'movies'
 urlpatterns = [
-    path('movies/', views.movie_list),
-    path('movies/<int:movie_pk>/', views.movie_detail),
-    # path('movies/<int:movie_pk>/like/', views.movie_like),
+    path('', views.movie_list),
+    path('<int:movie_pk>/', views.movie_detail),
+    path('<int:movie_pk>/like/', views.movie_like),
     # path('recommended/', views.recommended, name='recommended'),
 ]
