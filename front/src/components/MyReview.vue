63d5704de7c6736ef3b609ca2ebc9a1b03221f55
tmdwns950428 <tmdwns950428@naver.com>
2022-11-24 14:17:58 +0900

    Design-mypages complete

--- front/src/components/MyReview.vue
+++ front/src/components/MyReview.vue
@@ -1,8 +1,11 @@
 <template>
-  <div>
     <!-- 테이블 꾸미기 -->
-    {{review}}
-  </div>
+      <tr class="myReview_tr">
+        <th style="width: 10%" scope="row">{{review.user.username}}</th>
+        <td style="width: 45%">{{review.title}}</td>
+        <td style="width: 30%">{{review.movie.title}}</td>
+        <td style="width: 15%">{{review.created_at.slice(0, 10)}}</td>
+      </tr>
 </template>
 
 <script>
@@ -15,5 +18,20 @@ export default {
 </script>
 
 <style>
+  .reviews {
+    border-radius: 5px;
+    background-color: #1d1812;
+    width: 90%;
+    font-size: 18px;
+    text-align: left;
+  }
 
+  .table_body th,
+  .table_body td {
+    text-align: left;
+  }
+
+  .myReview_tr:hover {
+    opacity: 0.7;
+  }
 </style>
